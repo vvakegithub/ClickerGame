aa081d800b3044afb110965521291e6ba4233881
leonnik28 <lencruseckij@gmail.com>
2025-02-18 13:52:00 +0300

    Added Save Clicks

--- Assets/Scripts/Data/GameSession.cs
+++ Assets/Scripts/Data/GameSession.cs
@@ -1,19 +1,23 @@
 using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 
-public class GameSession : MonoBehaviour, IDisposable
+public class GameSession : MonoBehaviour
 {
     public UserData Data => _userData;
 
     [SerializeField] private Connection _connection;
 
     private UserData _userData;
+    private string _filePath;
 
     private void Awake()
     {
-        _userData.Money = _connection.GetScore();
+        _filePath = Path.Combine(Application.persistentDataPath, "userData.json");
+        LoadUserData();
+        //_userData.Money = _connection.GetScore();
     }
 
     private void Start()
@@ -24,24 +28,47 @@ public class GameSession : MonoBehaviour, IDisposable
         }
     }
 
-    public void Dispose()
+    public void OnDestroy()
     {
-        _connection.SendScore(_userData.Money);
+       // _connection.SendScore(_userData.Money);
+        SaveUserData();
     }
 
     public void UpdateMoney(int money)
     {
         _userData.Money += money;
+        SaveUserData();
     }
 
     private void AwardReferralBonus(string referralId)
     {
-        // TODO: Implement logic to award a bonus to the referrer
+        // Implement the referral bonus logic here
+    }
+
+    private void SaveUserData()
+    {
+        string json = JsonUtility.ToJson(_userData);
+        File.WriteAllText(_filePath, json);
+    }
+
+    private void LoadUserData()
+    {
+        if (File.Exists(_filePath))
+        {
+            string json = File.ReadAllText(_filePath);
+            _userData = JsonUtility.FromJson<UserData>(json);
+        }
+        else
+        {
+            _userData = new UserData();
+        }
     }
 }
 
+[System.Serializable]
 public struct UserData
 {
     public int Money;
     public int Referals;
 }
+
